b0a50b31c9521c235bd8aabcd41a2ec3f22762a8
Rohit0174 <maheshwari.rohit864@gmail.com>
2024-11-02 17:22:51 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 // require('dotenv').config({path:"./env"})
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/index.js";
-const app = express();
+import { app } from "./app.js"; //THIS APP WILL BE IMPORTED FROM APP.JS
 
-dotenv.config({path:"./env"})
+dotenv.config({ path: "./env" });
 
 // METHOD 2 THROUGH OTHER FILE
-connectDB()   //THIS IS A ASYNC FUNCTION SO IT WILL RETURN A PROMISE
-.then(()=>{
-  app.listen(process.env.PORT||8000,()=>{
-    console.log(`Server is running at PORT ${process.env.PORT}`)
+connectDB() //THIS IS A ASYNC FUNCTION SO IT WILL RETURN A PROMISE
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT ${process.env.PORT}`);
+    });
   })
-})
-.catch((err)=>console.log("MONGODB connection failed",err))
-
-
-
-
-
-
-
+  .catch((err) => console.log("MONGODB connection failed", err));
 
 /*   ------------------------------------------------APPROACH 1 TO CONNECT WITH DB ------------------------------------------------
 ;(async()=>{
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,10 @@
 //THIS IS PROMISES METHOD
-const asyncHandler = (requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-    }
-}
-export {asyncHandler};
-
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asyncHandler };
 
 // THIS IS TRY CATCH METHOD
 
